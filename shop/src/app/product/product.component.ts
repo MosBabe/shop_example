a0a001e9ddef21b441aceaabb744eaf8b64cbe9e
MosBabe <mosmankoc12@gmail.com>
2022-07-25 15:12:13 +0300

    Add ProductService and CategoryService

--- shop/src/app/product/product.component.ts
+++ shop/src/app/product/product.component.ts
@@ -1,75 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
 import { AlertifyService } from '../services/alertify.service';
+import { ProductService } from '../services/product.service';
+
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css'],
+  providers: [ProductService]
 })
 export class ProductComponent implements OnInit {
-  constructor(private alertifyService: AlertifyService) {}
+  constructor(private alertifyService: AlertifyService, private productService:ProductService) { }
 
   title = 'Ürün Listesi';
   filterText = '';
-  products: Product[] = [
-    {
-      id: 1,
-      name: 'Laptop',
-      price: 2500,
-      categoryId: 1,
-      description: 'Asus Zenbook',
-      imageUrl:
-        'https://images.unsplash.com/photo-1593642702821-c8da6771f0c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80',
-    },
-    {
-      id: 1,
-      name: 'Mouse',
-      price: 25,
-      categoryId: 2,
-      description: 'A4 Tech',
-      imageUrl:
-        'https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1167&q=80',
-    },
-    {
-      id: 1,
-      name: 'Laptop',
-      price: 2500,
-      categoryId: 1,
-      description: 'Asus Zenbook',
-      imageUrl:
-        'https://images.unsplash.com/photo-1593642702821-c8da6771f0c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80',
-    },
-    {
-      id: 1,
-      name: 'Mouse',
-      price: 25,
-      categoryId: 2,
-      description: 'A4 Tech',
-      imageUrl:
-        'https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1167&q=80',
-    },
-    {
-      id: 1,
-      name: 'Laptop',
-      price: 2500,
-      categoryId: 1,
-      description: 'Asus Zenbook',
-      imageUrl:
-        'https://images.unsplash.com/photo-1593642702821-c8da6771f0c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80',
-    },
-    {
-      id: 1,
-      name: 'Mouse',
-      price: 25,
-      categoryId: 2,
-      description: 'A4 Tech',
-      imageUrl:
-        'https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1167&q=80',
-    },
-  ];
+  products: Product[];
 
-  ngOnInit(): void {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(data=>{
+      this.products = data;
+    })
+  }
 
   addToCard(product: Product) {
     this.alertifyService.success(product.name + ' added');
